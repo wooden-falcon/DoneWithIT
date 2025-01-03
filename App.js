a6bed3c07a9f771570dd29eb7e0d7f7afbbb7c86
MONARCH <westk4388@gmail.com>
2025-01-03 10:00:10 +0100

    first part

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
-  return 
-    <WelcomeScreen />;
+  return <WelcomeScreen />;
   }
 
 
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 
 function WelcomeScreen(props) {
   return (
-    <ImageBackground source={require("../assets/pexels-njeromin-19400412.jpg")}>
+    <ImageBackground source={require("../assets/splash-icon.png")}>
+      <view style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/icon.png")} />
+        <Text>Sell What you dont need</Text>
+      </view>
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -14,7 +18,8 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
   background:{
     flex: 1,
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   loginButton:{
     width: '100%',
@@ -25,6 +30,15 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 70,
     backgroundColor: '#4ecdc4',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
   },
 })
 
